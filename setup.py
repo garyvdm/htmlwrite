3fb16c061afff7d99fd434a6805099bb8f442748
Gary van der Merwe <garyvdm@gmail.com>
2015-06-22 15:45:33 +0200

    Use pbr.

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 
-import setuptools
-import subprocess
+from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
-
-version = subprocess.check_output(['git', 'describe']).decode().strip()
-
-setuptools.setup(
-    name='htmlwrite',
-    version=version,
-    description='Write html to a file like object, using a pythonic syntax.',
-    long_description=readme,
-    url='https://github.com/garyvdm/htmlwrite',
-    license='MIT',
-    author='Gary van der Merwe',
-    author_email='garyvdm@gmail.com',
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
     py_modules=['htmlwrite', ],
     include_package_data=True,
     install_requires=['MarkupSafe', ],
     test_suite='test_htmlwrite',
 )
+
